8ca4be6890d42ab8b989b290870aa2a0ecff9429
Christian <chrille_0313@hotmail.com>
2022-10-20 01:58:14 +0200

    Add new events and update arguments to event

--- src/Event/Event.py
+++ src/Event/Event.py
@@ -1,10 +1,18 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class Event:
     class EventType(Enum):
-        GAME_UPDATE = 0
+        APP_UPDATE = auto()
+        MOVE_UP = auto()
+        MOVE_DOWN = auto()
+        MOVE_LEFT = auto()
+        MOVE_RIGHT = auto()
+        ZOOM_IN = auto()
+        ZOOM_OUT = auto()
+        PAUSE = auto()
 
-    def __init__(self, event_type, **kwargs):
+    def __init__(self, event_type, *args, **kwargs):
         self.type = event_type
+        self.args = args
         self.kwargs = kwargs

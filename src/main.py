6c4953b0fb7cae5a966510c3e37cf6ae1a0cb430
Christian <chrille_0313@hotmail.com>
2022-10-20 01:59:22 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+import pygame
+from Camera import Camera
+from Controller.App import App
+from Model.Space import Space
+from View.SpaceView import SpaceView
 
+from config import WIDTH, HEIGHT
 
 if __name__ == '__main__':
-    pass
+    camera = Camera(WIDTH, HEIGHT, (0, 0))
+    space_model = Space(WIDTH, HEIGHT, 10)
+    space_view = SpaceView(space_model)
+    
+    app = App(space_model, space_view, camera)
+    app.run()

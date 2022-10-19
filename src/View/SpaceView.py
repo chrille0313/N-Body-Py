63196a15190d32343f50fc74136ea874908ac2bf
Christian <chrille_0313@hotmail.com>
2022-10-20 01:55:33 +0200

    Create SpaceView.py

---
+++ src/View/SpaceView.py
@@ -0,0 +1,12 @@
+from pygame import surface
+from Model.Space import Space
+from View.BodyView import BodyView
+
+
+class SpaceView:
+    def __init__(self, space_model: Space):
+        self.space_model = space_model
+
+    def render(self, camera):
+        for body in self.space_model.bodies:
+            BodyView.render(camera, body)
